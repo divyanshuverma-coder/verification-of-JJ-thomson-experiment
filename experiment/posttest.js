7b58be84e851b4f94b04abfac9e2770e5be78407
divyanshuverma-coder <56770788+divyanshuverma-coder@users.noreply.github.com>
2019-11-01 00:30:27 +0530

    Create posttest.js

---
+++ experiment/posttest.js
@@ -0,0 +1,30 @@
+
+const myQuestions = [
+
+    {
+
+      question: "1.	What is the value of e/m??",
+
+      answers: {
+
+        a: "	1.75*10^11 C/Kg",
+
+        b: "	1.75*10^11 CKg ",
+
+        c: "	1.75*10^11 Kg",
+
+        d: "  1.75*10^11 C",
+
+      },
+
+      correctAnswer: "a"
+
+    },
+
+ 
+
+    
+
+    
+
+  ];
