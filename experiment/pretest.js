b06dcc490bcf372b5dcaaf7909583a32d6f71c96
divyanshuverma-coder <56770788+divyanshuverma-coder@users.noreply.github.com>
2019-11-01 00:35:24 +0530

    Update pretest.js

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -1,1 +1,30 @@
 
+const myQuestions = [
+
+    {
+
+      question: "1.	What is the expression of the electric force on electron when it enters in a electric field?",
+
+      answers: {
+
+        a: "	F=eE ",
+
+        b: "	F=ev",
+
+        c: "	F=eB",
+
+        d: "	F=be",
+
+      },
+
+      correctAnswer: "a"
+
+    },
+
+ 
+
+    
+
+    
+
+  ];
